47c3a55658f81787ed4e18ad1aab35f1999e0b10
Adriana Hernández R <adriana.hernandez@vass.es>
2024-07-25 14:15:15 +0200

    optimize webpack

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { createRoot } from 'react-dom/client';
-import Map from './Map/Map';
+// import Map from './Map/Map';
 import 'leaflet/dist/leaflet.css';
+import './main.css';
+
+// Lazy load the Map component
+const Map = lazy(() => import('./Map/Map'));
+
+const Loading = () => {
+  return <div>Loading...</div>;
+}
 
 const root = createRoot(document.getElementById('app'));
-// render Map component
-root.render(<Map />);
+
+// render Map component with Suspense fallback
+root.render(
+  <Suspense fallback={<Loading />}>
+    <Map />
+  </Suspense>
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, argv) => {
   const isDevelopment = argv.mode === 'development';
@@ -8,7 +12,8 @@ module.exports = (env, argv) => {
     entry: './src/index.js',
     output: {
       path: path.resolve(__dirname, 'dist'),
-      filename: 'app.js',
+      filename: '[name].[contenthash].js',
+      clean: true,
     },
     module: {
       rules: [
@@ -34,7 +39,48 @@ module.exports = (env, argv) => {
     },
     devtool: isDevelopment ? 'eval-source-map' : 'source-map',
     plugins: [
-      new Dotenv() // This will load the .env file and make it available to the app
+      new Dotenv(), // This will load the .env file and make it available to the app
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        inject: 'body', // Inject the script and styles tag in the body, ensuring the load of the assets with the unique generated name by the content hash.
+      }),
+      // This plugin compress the files with gzip
+      new CompressionWebpackPlugin({
+        algorithm: 'gzip',
+        test: /\.js$|\.css$|\.html$/,
+        threshold: 10240,
+        minRatio: 0.8,
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: 'favicon.ico', to: '' }, // Copy the favicon.ico to the dist folder
+        ],
+      }),
     ],
+    optimization: {
+      // Configure the code splitting to ensure that the files are not bigger than an specific size.
+      splitChunks: {
+        chunks: 'all',
+        maxSize: 243 * 1024, // 244 KiB
+      },
+
+      // This TerserPlugin minify the js files and remove the console.log and other elements.
+      minimize: !isDevelopment,
+      minimizer: [new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+        },
+      })],
+    },
+
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, 'dist'),
+      },
+      compress: true,
+      port: 9000,
+    },
   };
 };
