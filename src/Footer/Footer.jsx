8c9a3cdfec6f9d92f63991f6f2117148a1f4503a
Adriana Hernández R <adriana.hernandez@vass.es>
2024-07-27 02:14:10 +0200

    remove 'music' from footer

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
       <div className="footer-content">
         <div className="what-is-this">
           <span className="i1">Which Youtube channels are most popular now (realtime)</span>
-          <span className="i2">Data obtained from the YouTube API - You are viewing the #1 trending music video channel from each country.</span>
+          <span className="i2">Data obtained from the YouTube API - You are viewing the #1 trending video channel from each country.</span>
         </div>
         <span className="developer">
           © Developed by <a target="_blank" href="https://github.com/adrianahdez">Adriana Hernández</a>
