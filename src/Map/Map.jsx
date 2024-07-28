f805b5ea37b17d0496531dc6b1bad52206616ff8
Adriana Hernández R <adriana.hernandez@vass.es>
2024-07-28 15:49:50 +0200

    modify zoom

--- src/Map/Map.jsx
+++ src/Map/Map.jsx
@@ -76,7 +76,7 @@ export default function Map({ category, toggleSidebar, mapPoint, setMapPoint }) 
   const mapConfig = {
     center: [0, 0],
     zoom: 3,
-    minZoom: 2,
+    minZoom: 1,
     maxZoom: 5,
     scrollWheelZoom: true,
     zoomControl: false,
