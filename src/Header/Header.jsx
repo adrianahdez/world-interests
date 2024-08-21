5f128cffa20fa61ded24b5b401a6f09085ad2a57
Adriana Hernández R <adriana.hernandez@vass.es>
2024-08-21 18:40:43 +0200

    feat: add header with styles

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -22,13 +22,18 @@ export default function Header({ isDialogOpen, toggleDialog }) {
   // Return a burger menu icon
   return (
     <header className='header'>
-      <div className={`menu-toggle${isDialogOpen === true ? ' close-icon' : ''}`}>
+      <div className={`menu-item menu-toggle${isDialogOpen === true ? ' close-icon' : ''}`}>
         <div className='toggle-btn' onClick={toggleDialog}>
           <span></span>
           <span></span>
           <span></span>
         </div>
       </div>
+      {/* <div className={`menu-item menu-item__lang${isEs === true ? ' es' : ' en'}`}>
+        <div className='toggle-btn' onClick={toggleLang}>
+          <span>{isEs === true ? 'ES' : 'EN'}</span>
+        </div>
+      </div> */}
     </header>
   );
 }
