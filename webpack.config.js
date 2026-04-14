25523d558d8d5271aa31d3e72c6c1e66becf1e9e
Adriana Hernández Regueiro <adriana.hregueiro@gmail.com>
2026-04-14 13:49:32 +0200

    WIF-3: step 3 - copy _headers to dist via CopyWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -65,6 +65,7 @@ module.exports = (env, argv) => {
           { from: 'sitemap.xml', to: '' }, // Copy the sitemap.xml to the dist folder
           { from: 'screenshot.jpg', to: '' }, // Copy the screenshot.jpg to the dist folder
           { from: '_redirects', to: '' }, // Copy the redirects file to the dist folder
+          { from: '_headers', to: '' }, // Copy the headers file to the dist folder
         ],
       }),
     ],
